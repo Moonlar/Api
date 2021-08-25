257d7aa54f285246ca119d32ef942cfae22b33b7
Miguel Ângelo <miguelcg958@gmail.com>
2021-08-24 21:01:35 -0300

    chore: Created CreatePrductSchema

--- src/utils/Validators.ts
+++ src/utils/Validators.ts
@@ -25,3 +25,30 @@ export const AdminUserLogInSchema = yup.object().shape({
   email: yup.string().required().email(),
   password: yup.string().required().min(6).max(20),
 });
+
+export const CreateProductSchema = yup.object().shape({
+  title: yup.string().required().min(4).max(30),
+  description: yup.string().required().min(4).max(150),
+  image_url: yup.string().url(),
+  server: yup.string().required(),
+  price: yup.number().required().min(1),
+  benefits: yup
+    .array()
+    .of(
+      yup.object().shape({
+        title: yup.string().required().min(4).max(30),
+        description: yup.string().required().min(4).max(60),
+      })
+    )
+    .required(),
+  commands: yup
+    .array()
+    .of(
+      yup.object().shape({
+        title: yup.string().required().min(4).max(30),
+        description: yup.string().required().min(4).max(60),
+        command: yup.string().required().min(4).max(60),
+      })
+    )
+    .required(),
+});
