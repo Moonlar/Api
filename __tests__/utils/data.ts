668b57bd745a86c86dc8c0df16792f3a13100aaf
Miguel Ângelo <miguelcg958@gmail.com>
2021-09-04 11:14:16 -0300

    chore: Started testing for product routes

--- __tests__/utils/data.ts
+++ __tests__/utils/data.ts
@@ -3,9 +3,10 @@ import { v4 as uuid } from 'uuid';
 import conn from '../../src/database/Connection';
 import Password from '../../src/utils/Password';
 
-let alreadyExecuted = {
+const alreadyExecuted = {
   users: false,
   servers: false,
+  products: false,
 };
 
 const usersData = [
@@ -52,6 +53,70 @@ export const serversData = [
   },
 ];
 
+/**
+ * [0] - Deleted
+ * [1] - Disabled
+ * [2] - Active
+ */
+export const productsData = [
+  {
+    id: uuid(),
+    name: 'deleted_1',
+    description: 'deleted_1',
+    server_id: serversData[1].id,
+    price: 50,
+    active: false,
+    deleted_at: conn.fn.now(),
+  },
+  {
+    id: uuid(),
+    name: 'Product 1',
+    description: 'Product 1',
+    server_id: serversData[1].id,
+    price: 50,
+    active: false,
+  },
+  {
+    id: uuid(),
+    name: 'Product 2',
+    description: 'Product 2',
+    server_id: serversData[1].id,
+    price: 50,
+    active: true,
+  },
+];
+
+const getProductRelated = (id: string) => ({
+  benefits: [
+    {
+      id: uuid(),
+      product_id: id,
+      name: 'Benefit 1',
+      description: 'Benefit 1',
+    },
+    {
+      id: uuid(),
+      product_id: id,
+      name: 'Benefit 2',
+      description: 'Benefit 2',
+    },
+  ],
+  commands: [
+    {
+      id: uuid(),
+      product_id: id,
+      name: 'Command 1',
+      command: 'command 1',
+    },
+    {
+      id: uuid(),
+      product_id: id,
+      name: 'Command 2',
+      command: 'command 2',
+    },
+  ],
+});
+
 export const createDefaultUsers = () => {
   if (process.env.NODE_ENV !== 'test' || alreadyExecuted.users) return;
 
@@ -66,4 +131,28 @@ export const createDefaultServers = () => {
   alreadyExecuted.servers = true;
 
   return conn('servers').insert(serversData);
+};
+
+export const createDefaultProducts = () => {
+  if (process.env.NODE_ENV !== 'test' || alreadyExecuted.products) return;
+
+  alreadyExecuted.products = true;
+
+  const benefits = [],
+    commands = [];
+
+  productsData.forEach(({ id }, index) => {
+    if (index === 0) return;
+
+    const data = getProductRelated(id);
+
+    benefits.push(...data.benefits);
+    commands.push(...data.commands);
+  });
+
+  return Promise.all([
+    conn('products').insert(productsData),
+    conn('products_benefits').insert(benefits),
+    conn('products_commands').insert(commands),
+  ]);
 };
--- __tests__/utils/schemas.ts
+++ __tests__/utils/schemas.ts
@@ -79,3 +79,112 @@ export const serverSchema = {
     'updated_at',
   ],
 };
+
+export const productSchema = {
+  $id: 'productTestSchema',
+  type: 'object',
+  properties: {
+    id: {
+      type: 'string',
+      require: true,
+    },
+    name: {
+      type: 'string',
+      require: true,
+    },
+    description: {
+      type: 'string',
+      require: true,
+    },
+    image_url: {
+      type: ['string', 'null'],
+      require: true,
+    },
+    price: {
+      type: 'number',
+      require: true,
+    },
+    active: {
+      type: 'boolean',
+      require: true,
+    },
+    created_at: {
+      type: 'string',
+      require: true,
+    },
+    updated_at: {
+      type: 'string',
+      require: true,
+    },
+  },
+  required: [
+    'id',
+    'name',
+    'description',
+    'image_url',
+    'price',
+    'active',
+    'created_at',
+    'updated_at',
+  ],
+};
+
+export const productServerSchema = {
+  $id: 'productTestSchema',
+  type: 'object',
+  properties: {
+    id: {
+      type: 'string',
+      require: true,
+    },
+    name: {
+      type: 'string',
+      require: true,
+    },
+    description: {
+      type: 'string',
+      require: true,
+    },
+  },
+  required: ['id', 'name', 'description'],
+};
+
+export const productBenefitSchema = {
+  $id: 'productTestSchema',
+  type: 'object',
+  properties: {
+    id: {
+      type: 'string',
+      require: true,
+    },
+    name: {
+      type: 'string',
+      require: true,
+    },
+    description: {
+      type: 'string',
+      require: true,
+    },
+  },
+  required: ['id', 'name', 'description'],
+};
+
+export const productCommandSchema = {
+  $id: 'productTestSchema',
+  type: 'object',
+  properties: {
+    id: {
+      type: 'string',
+      require: true,
+    },
+    name: {
+      type: 'string',
+      require: true,
+    },
+    command: {
+      type: 'string',
+      require: true,
+    },
+  },
+  required: ['id', 'name', 'command'],
+};
