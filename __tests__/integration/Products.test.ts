0ea4f6116eee8488785554761fc70c079baec595
Miguel Ângelo <miguelcg958@gmail.com>
2021-09-05 19:48:17 -0300

    chore: Completed tests to list products

--- __tests__/integration/Products.test.ts
+++ __tests__/integration/Products.test.ts
@@ -50,5 +50,85 @@ describe('Products Routes', () => {
         expect(product.server).toMatchSchema(productServerSchema);
       });
     });
+
+    it('(Admin) Deve retornar dados válidos', async () => {
+      const response = await adminAgent.get('/products');
+
+      expect(response.statusCode).toBe(200);
+      expect(response.headers).toBeTruthy();
+      expect(response.headers['content-type']).toMatch(/json/);
+      expect(response.body).toBeTruthy();
+      expect(response.body).toHaveProperty('page', 1);
+      expect(response.body).toHaveProperty('total_pages', 1);
+      expect(response.body).toHaveProperty('total_products', 2);
+      expect(response.body).toHaveProperty('limit', 10);
+      expect(response.body).toHaveProperty('products');
+      expect(Array.isArray(response.body.products)).toBeTruthy();
+
+      response.body.products.forEach((product: any) => {
+        expect(product).toMatchSchema(productSchema);
+        expect(product.server).toMatchSchema(productServerSchema);
+        expect(product).toHaveProperty('active');
+      });
+    });
+
+    it('(Manager) Deve retornar dados válidos', async () => {
+      const response = await managerAgent.get('/products');
+
+      expect(response.statusCode).toBe(200);
+      expect(response.headers).toBeTruthy();
+      expect(response.headers['content-type']).toMatch(/json/);
+      expect(response.body).toBeTruthy();
+      expect(response.body).toHaveProperty('page', 1);
+      expect(response.body).toHaveProperty('total_pages', 1);
+      expect(response.body).toHaveProperty('total_products', 2);
+      expect(response.body).toHaveProperty('limit', 10);
+      expect(response.body).toHaveProperty('products');
+      expect(Array.isArray(response.body.products)).toBeTruthy();
+
+      response.body.products.forEach((product: any) => {
+        expect(product).toMatchSchema(productSchema);
+        expect(product.server).toMatchSchema(productServerSchema);
+        expect(product).toHaveProperty('active');
+      });
+    });
+
+    it('Deve retornar dados válidos com parâmetros de busca (page)', async () => {
+      const response = await request.get('/products').query({ page: 100 });
+
+      expect(response.statusCode).toBe(200);
+      expect(response.headers).toBeTruthy();
+      expect(response.headers['content-type']).toMatch(/json/);
+      expect(response.body).toBeTruthy();
+      expect(response.body).toHaveProperty('page', 1);
+      expect(response.body).toHaveProperty('total_pages', 1);
+      expect(response.body).toHaveProperty('total_products', 1);
+      expect(response.body).toHaveProperty('limit', 10);
+      expect(response.body).toHaveProperty('products');
+      expect(Array.isArray(response.body.products)).toBeTruthy();
+
+      response.body.products.forEach((product: any) => {
+        expect(product).toMatchSchema(productSchema);
+        expect(product.server).toMatchSchema(productServerSchema);
+      });
+    });
+
+    it('Deve retornar dados válidos com parâmetros de busca (search)', async () => {
+      const response = await request
+        .get('/products')
+        .query({ search: 'invalid' });
+
+      expect(response.statusCode).toBe(200);
+      expect(response.headers).toBeTruthy();
+      expect(response.headers['content-type']).toMatch(/json/);
+      expect(response.body).toBeTruthy();
+      expect(response.body).toHaveProperty('page', 1);
+      expect(response.body).toHaveProperty('total_pages', 1);
+      expect(response.body).toHaveProperty('total_products', 0);
+      expect(response.body).toHaveProperty('limit', 10);
+      expect(response.body).toHaveProperty('products');
+      expect(Array.isArray(response.body.products)).toBeTruthy();
+      expect(response.body.products.length).toBe(0);
+    });
   });
 });
--- __tests__/utils/schemas.ts
+++ __tests__/utils/schemas.ts
@@ -105,7 +105,7 @@ export const productSchema = {
       require: true,
     },
     active: {
-      type: 'boolean',
+      type: 'number',
       require: true,
     },
     created_at: {
